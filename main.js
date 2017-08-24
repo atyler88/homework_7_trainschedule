32b6bc4215faa239fc5595d0e1d05ff9c811bdc3
aaron <a.tyler88@gmail.com>
2017-08-23 23:15:28 -0500

    initialized firebase

---
+++ main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyATiGOGhTkR3h5aNGKV5gMTAI4m6ja0TBc",
+    authDomain: "trainschedule-41713.firebaseapp.com",
+    databaseURL: "https://trainschedule-41713.firebaseio.com",
+    projectId: "trainschedule-41713",
+    storageBucket: "trainschedule-41713.appspot.com",
+    messagingSenderId: "342610437300"
+  };
+  firebase.initializeApp(config);
